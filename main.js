45d8e8f228d177682dbd76ee3774e427ac76246e
Jeshurungamer <106802924+Jeshurungamer@users.noreply.github.com>
2022-06-03 18:13:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("My first console project");
+var first_name="Ranbir";
+var last_name="kapoor";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var a=5;
+var b=10;
+var add=a+b;
+console.log(add);
+var sub=b-a;
+console.log(sub);
+var multi=a*b;
+console.log(multi);
+var div=b/a;
+console.log(div);
